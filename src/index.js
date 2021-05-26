4da2fe52c5937377133262ae714a671a8d810e64
Kittybot <kateowynn@gmail.com>
2021-05-25 21:11:15 -0700

    updated current temp icons

--- src/index.js
+++ src/index.js
@@ -79,6 +79,63 @@ function forecastSearch(coordinates) {
 }
 
 //Changing HTML
+function updateIcon(currentIcon) {
+  if (currentIcon === "01d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/601/original/01d.png?1621999754";
+  }
+  if (currentIcon === "01n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/602/original/01n.png?1621999763";
+  }
+  if (currentIcon === "02d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/603/original/02d.png?1621999770";
+  }
+  if (currentIcon === "02n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/604/original/02n.png?1621999779";
+  }
+  if (currentIcon === "03d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/605/original/03d.png?1621999786";
+  }
+  if (currentIcon === "03n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/606/original/03n.png?1621999793";
+  }
+  if (currentIcon === "04d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/607/original/04d.png?1621999803";
+  }
+  if (currentIcon === "04n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/608/original/04n.png?1621999811";
+  }
+  if (currentIcon === "09d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/609/original/09d.png?1621999824";
+  }
+  if (currentIcon === "09n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/610/original/09n.png?1621999831";
+  }
+  if (currentIcon === "10d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/611/original/10d.png?1621999840";
+  }
+  if (currentIcon === "10n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/612/original/10n.png?1621999850";
+  }
+  if (currentIcon === "11d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/613/original/11d.png?1621999861";
+  }
+  if (currentIcon === "11n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/614/original/11n.png?1621999870";
+  }
+  if (currentIcon === "13d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/615/original/13d.png?1621999879";
+  }
+  if (currentIcon === "13n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/616/original/13n.png?1621999890";
+  }
+  if (currentIcon === "50d") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/617/original/50d.png?1621999898";
+  }
+  if (currentIcon === "50n") {
+    return "https://s3.amazonaws.com/shecodesio-production/uploads/files/000/009/618/original/50n.png?1621999907";
+  }
+}
+
 function updateWeather(response) {
   document.querySelector(
     "#location"
@@ -98,7 +155,9 @@ function updateWeather(response) {
   ).innerHTML = `${response.data.weather[0].description}`;
   document.querySelector(
     "#temperature-icon"
-  ).innerHTML = `<img src="http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png"/>`;
+  ).innerHTML = `<img src="${updateIcon(
+    response.data.weather[0].icon
+  )}" width=200/>`;
   document
     .querySelector("#temperature-icon")
     .setAttribute("alt", response.data.weather[0].description);
